1f8bffd8fa83c5d8f3b7cebcee29001300d5de97
Владислав Урбанский <zerdonik777@gmail.com@>
2024-06-19 09:19:37 +0300

    added new fields for model userProfile and edited serializers

--- arendaby-backend/arendaby/arendaby/urls.py
+++ arendaby-backend/arendaby/arendaby/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('api/', include('apartment.urls')),
     path('api-auth/', include('rest_framework.urls')),
 
-    path('api/token', UserLoginView.as_view(), name='get_token'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/', UserLoginView.as_view(), name='get_token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- arendaby-backend/arendaby/user/urls.py
+++ arendaby-backend/arendaby/user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import UserRegisterView
+from .views import UserRegisterView, UsersListView, UsersProfileListView, UserDetailView
 
 urlpatterns = [
     path('user/register/', UserRegisterView.as_view(), name='register'),
-    # path('user/list/', GetUserListView.as_view(), name='user-list'),
+    path('user/list/', UsersListView.as_view(), name='user-list'),
+    path('user-profile/list/', UsersProfileListView.as_view(), name='user-profile-list'),
+    path('user/<int:pk>', UserDetailView.as_view(), name='user-profile-list'),
 ]
