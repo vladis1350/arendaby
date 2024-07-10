6738500ffc26b5d01e5bc55a19d8e5891d9dab5b
Владислав Урбанский <zerdonik777@gmail.com@>
2024-07-10 11:11:51 +0300

    added new model Idea and views and serializer for him

--- arendaby-backend/arendaby/housing/serializers.py
+++ arendaby-backend/arendaby/housing/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import MenuItem, VocationIdeas
+from .models import MenuItem, VocationIdeas, Idea, Region
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
@@ -13,3 +13,19 @@ class VocationIdeasSerializer(serializers.ModelSerializer):
     class Meta:
         model = VocationIdeas
         fields = '__all__'
+
+
+class IdeaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Idea
+        fields = '__all__'
+
+
+class RegionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Region
+        fields = '__all__'
+
+
+class IdeaTextSerializer(serializers.Serializer):
+    long_text = serializers.CharField(style={'base_template': 'textarea.html'})
--- arendaby-backend/arendaby/housing/urls.py
+++ arendaby-backend/arendaby/housing/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
-from .views import MenuItemList, VocationIdeasList, UpdateMenuItem
+from .views import MenuItemList, VocationIdeasList, UpdateMenuItem, IdeasList, TextListView
 
 urlpatterns = [
     path('menu/', MenuItemList.as_view(), name='menu-list'),
     path('menu/update', UpdateMenuItem.as_view(), name='menu-update'),
-    path('ideas/', VocationIdeasList.as_view(), name='ideas-list'),
+    path('v-ideas/', VocationIdeasList.as_view(), name='ideas-list'),
+    path('v-ideas/idea/<int:v_id>', IdeasList.as_view(), name='ideas'),
+    path('v-ideas/parser', TextListView.as_view(), name='ideas'),
     path('api-auth/', include('rest_framework.urls'))
 ]
